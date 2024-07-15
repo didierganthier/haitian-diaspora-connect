e45a6fbb6c0d5dfd32ff509c308f4f013bbb067f
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-07-15 14:15:24 -0400

    Basic template

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { HaitianDiasporaConnect } from "@/components/haitian-diaspora-connect";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <HaitianDiasporaConnect />
+  );
+}
