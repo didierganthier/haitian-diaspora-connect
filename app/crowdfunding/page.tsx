28de1940bcddee72786aa9cc7a5bb8a90da46584
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-07-16 11:01:13 -0400

    Add crowdfunding page

---
+++ app/crowdfunding/page.tsx
@@ -0,0 +1,10 @@
+import { CrowdFundingComponent } from '@/components/crowd-funding-component'
+import React from 'react'
+
+const Crowdfunding = () => {
+  return (
+    <CrowdFundingComponent />
+  )
+}
+
+export default Crowdfunding
---
+++ app/forum/page.tsx
@@ -0,0 +1,10 @@
+import { ForumComponent } from '@/components/forum-component'
+import React from 'react'
+
+const Forum = () => {
+  return (
+    <ForumComponent />
+  )
+}
+
+export default Forum
