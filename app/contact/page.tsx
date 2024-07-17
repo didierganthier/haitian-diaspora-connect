81d51e938aa48221657cfac83b73f90ddd7e8bde
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-07-17 11:16:29 -0400

    Design Contact and Join components

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { ContactComponent } from '@/components/contact-component'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <ContactComponent />
+  )
+}
+
+export default Contact
---
+++ app/join/page.tsx
@@ -0,0 +1,10 @@
+import { JoinComponent } from '@/components/join-component'
+import React from 'react'
+
+const Join = () => {
+  return (
+    <JoinComponent />
+  )
+}
+
+export default Join
